129fd04859a06d34383269fca557a2521fb61ef7
Jean Mallmann <jean.mallmann.pb@compasso.com.br>
2025-01-24 17:22:18 -0300

    Validação de 'UsuarioCreateDTO'.

--- src/main/java/com/repository/RepositorioUsuario.java
+++ src/main/java/com/repository/RepositorioUsuario.java
@@ -4,7 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.entities.Usuario;
 
-public interface RepositorioUsuario extends JpaRepository<Usuario, Long>
-{
-  
-}
+public interface RepositorioUsuario extends JpaRepository<Usuario, Long> {}
